58c26e3d67351dac361a7c8dd78876b45cadcb6e
Langat <davidsonlangat2021@gmail.com>
2024-04-24 11:28:35 +0300

    initial commit for products page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,13 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { ProductCategoriesComponent } from "./product-categories/product-categories.component";
-import { SidebarFiltersComponent } from "./sidebar-filters/sidebar-filters.component";
+import { SidebarFiltersComponent } from "./products-sidebar/sidebar-filters.component";
 import { ProductPostComponent } from "./product-post/product-post.component";
-import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [
-    FooterComponent,
     RouterOutlet,
     NavbarComponent,
     ProductCategoriesComponent,
